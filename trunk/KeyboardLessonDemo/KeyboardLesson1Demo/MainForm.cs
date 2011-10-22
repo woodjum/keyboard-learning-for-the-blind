bf46619d7f7f8448112d829f4dbe22215ed89133
Jessica.O.Leung@gmail.com <Jessica.O.Leung@gmail.com@0eb8ec76-9b3b-03ec-b8e3-7bfdb06bd885>
2011-10-22 07:51:58 +0000

    Added silent lesson selection menu for Learning Project

--- trunk/KeyboardLessonDemo/KeyboardLesson1Demo/MainForm.cs
+++ trunk/KeyboardLessonDemo/KeyboardLesson1Demo/MainForm.cs
@@ -14,7 +14,7 @@ namespace KeyboardLesson
     {
         static String LESSON_FOLDER = "lessons";        
         String[] lessonFiles;                       // Array of lesson script files
-        int lessonIndex = 0;                        // lessonArray index
+
 
         public MainForm()
         {
@@ -22,7 +22,7 @@ namespace KeyboardLesson
             {
                 InitializeComponent();
                 Initialize();
-                RunCurrentLesson();
+                
             }
             catch (Exception ex)
             {
@@ -36,7 +36,29 @@ namespace KeyboardLesson
         {
             //find all avalible lesson files in the lessons directory
             lessonFiles = Directory.GetFiles(LESSON_FOLDER, "lesson*.txt");
-            Console.WriteLine("lessons found: " + lessonFiles.Length);   
+            Console.WriteLine("lessons found: " + lessonFiles.Length);
+   
+            //display lessons found in listbox
+            foreach (string lessonFile in lessonFiles)
+            {
+                //remove folder name and \ from the front of the filename
+                string lessonName = lessonFile.Substring(LESSON_FOLDER.Length + 1);
+
+                //remove the .txt from the end of the filename
+                lessonName = lessonName.Substring(0,lessonName.Length - 4);
+
+                //display the remaining filename as the lesson name
+                this.LessonListBox.Items.Add(lessonName);
+            }
+
+            //have the first lesson automatically selected
+            if (lessonFiles.Length > 0)
+            {
+                this.LessonListBox.SetSelected(0, true);
+            }
+
+            //attach hook for key presses
+            this.LessonListBox.KeyUp  += new KeyEventHandler(MainFrom_KeyUp);
         }
 
         private void RunCurrentLesson()
@@ -45,31 +67,48 @@ namespace KeyboardLesson
             this.Hide();
 
             //determine what lesson is the current one
+            int lessonIndex = this.LessonListBox.SelectedIndex;
             String lessonFile = lessonFiles[lessonIndex];
 
             //run the current lesson
             LessonForm lesson = new LessonForm(lessonFile);
             lesson.TopMost = true;
             lesson.LessonFinished += new EventHandler(this.LessonForm_LessonFinished);
-            lesson.Show();
-
-
+            lesson.ShowDialog(this);
+           
+           
             
         }
 
         //shows the next lesson after the first has finished
         private void LessonForm_LessonFinished(object sender, EventArgs e)
         {
+           
             this.Show();
 
-            //run the next lesson
-            this.lessonIndex++;
+            //grab the currently selected index
+            int lessonIndex = this.LessonListBox.SelectedIndex;
 
-            //only run the next lesson if there are any
-            if (lessonIndex < lessonFiles.Length)
-            {
-                RunCurrentLesson();
-            }
+            //select the next lesson with wrap around
+            this.LessonListBox.SelectedIndex = (lessonIndex +1) % this.lessonFiles.Length;
+         
+        }
+
+        //detects that the space key or the enter key was pressed 
+        //to run the current lesson
+        void MainFrom_KeyUp(object sender, KeyEventArgs e)
+        {
+            //Debug
+            Console.Out.WriteLine("Main Form Received key " + e.KeyCode.ToString());
+
+            //Known Bug: When the lesson form closes, the main form suddenly
+            //           receives all the keys pressed when the lesson form was open
+            //
+            if (Keys.Return.Equals(e.KeyCode) )
+           {
+               RunCurrentLesson();
+           }
+            
         }
     }
 }

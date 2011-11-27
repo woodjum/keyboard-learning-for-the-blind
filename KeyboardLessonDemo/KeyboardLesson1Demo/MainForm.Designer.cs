66e7601786cd8ed165d6310eaa551bc780e79fa7
Jessica.O.Leung <Jessica.O.Leung@gmail.com>
2011-11-27 00:20:03 +0000

    Added directions to main menu

--- KeyboardLessonDemo/KeyboardLesson1Demo/MainForm.Designer.cs
+++ KeyboardLessonDemo/KeyboardLesson1Demo/MainForm.Designer.cs
@@ -30,17 +30,18 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.LessonListBox = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(16, 15);
+            this.label1.Location = new System.Drawing.Point(16, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(252, 31);
+            this.label1.Size = new System.Drawing.Size(95, 31);
             this.label1.TabIndex = 0;
-            this.label1.Text = "主菜单 (main menu)";
+            this.label1.Text = "主菜单";
             // 
             // LessonListBox
             // 
@@ -50,11 +51,21 @@
             this.LessonListBox.Size = new System.Drawing.Size(234, 173);
             this.LessonListBox.TabIndex = 1;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "按回车键或空格键开始一个教训";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.LessonListBox);
             this.Controls.Add(this.label1);
             this.Name = "MainForm";
@@ -68,5 +79,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListBox LessonListBox;
+        private System.Windows.Forms.Label label2;
     }
 }

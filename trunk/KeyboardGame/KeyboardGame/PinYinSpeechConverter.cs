944630568637f0aadd1bb86da30e9f0a276a6442
Jessica.O.Leung@gmail.com <Jessica.O.Leung@gmail.com@0eb8ec76-9b3b-03ec-b8e3-7bfdb06bd885>
2012-05-17 01:22:40 +0000

    Added PinYin TTS Support To Game

---
+++ trunk/KeyboardGame/KeyboardGame/PinYinSpeechConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeyboardGame
+{
+    public static class PinYinSpeechConverter
+    {
+        public static string Convert(string input)
+        {
+            
+            KeySoundMapping mappings = KeySoundMapping.GetInstance();
+            StringBuilder convertedOutput = new StringBuilder();
+            StringBuilder subsegment = new StringBuilder();
+
+            subsegment.Append("<s>");
+
+            foreach (char c in input)
+            {
+                if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+                {
+                    if (subsegment == null)
+                    {
+                        subsegment = new StringBuilder();
+                    }
+                    subsegment.Append("</s>");
+                    subsegment.Append(mappings[c]);
+                    convertedOutput.Append(subsegment);
+                    subsegment = null;
+                }
+                else
+                {
+                    if (subsegment == null)
+                    {
+                        subsegment = new StringBuilder();
+                        subsegment.Append("<s>");
+                    }
+                    subsegment.Append(c);
+                }
+            }
+
+            if (subsegment != null)
+            {
+                subsegment.Append("</s>");
+                convertedOutput.Append(subsegment);
+            }
+
+            return convertedOutput.ToString();
+        }
+    }
+}
